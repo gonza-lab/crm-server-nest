ed015c510345a8f7457384a8e163622507baa8c5
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-01-14 13:53:48 -0300

    add validation when creates user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsInt, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString() first_name: string;
+  @IsString() last_name: string;
+  @IsEmail() email: string;
+  @IsString() password: string;
+  @IsString() country: string;
+  @IsString() city: string;
+  @IsString() locality: string;
+  @IsString() address: string;
+  @IsString() zip_code: string;
+  @IsInt() role: number;
+}
