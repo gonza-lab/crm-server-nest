0b7fe55892cca00acdf83dfd300cb37773e9aa7a
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-01-14 18:03:19 -0300

    add connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
@@ -8,6 +9,16 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_DATABASE || 'test',
+      entities: [],
+      synchronize: true,
+    }),
     AuthModule,
     OrderModule,
     UserModule,
