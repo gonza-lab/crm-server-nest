4f91cf65619201e9090fe245c896921ada33e4dc
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-01-15 21:33:29 -0300

    add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
-import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
-import { Order } from './order/entities/order.entity';
 import { RoleModule } from './role/role.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: +process.env.DB_PORT || 3306,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_DATABASE || 'test',
-      entities: [User, Order],
-      synchronize: process.env.NODE_ENV !== 'production',
-    }),
+    DatabaseModule,
     AuthModule,
     OrderModule,
     UserModule,
