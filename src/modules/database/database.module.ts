4e5f5105476d925d35e75fce0cc0eca58d0129c8
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-02-23 10:27:47 -0300

    add ormconfig json

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,31 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrderStatus } from '../order-status/entities/order-status.entity';
-import { Order } from '../order/entities/order.entity';
-import { ProductOrder } from '../product/entities/product-order.entity';
-import { Product } from '../product/entities/product.entity';
-import { Role } from '../role/entities/role.entity';
-import { User } from '../user/entities/user.entity';
 import { SeedsModule } from './seeds/seeds.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        entities: [Role, User, ProductOrder, Order, Product, OrderStatus],
-        synchronize: configService.get('NODE_ENV') !== 'production',
-      }),
-      inject: [ConfigService],
-    }),
-    SeedsModule,
-  ],
+  imports: [TypeOrmModule.forRoot({}), SeedsModule],
 })
 export class DatabaseModule {}
